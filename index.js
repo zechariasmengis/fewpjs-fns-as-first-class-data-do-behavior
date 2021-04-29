301952fed947afa43d7d139b330d3f2cc15ab8ce
Zecharias Mengis <zmengis@gmail.com>
2021-04-29 10:17:56 -0700

    Done.

--- index.js
+++ index.js
@@ -9,5 +9,20 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
+function greet(time) {
+  const parsedTime = parseInt(time)
+  if (parsedTime < 12) {
+    return "Good Morning"
+  } else if (parsedTime >= 12 && parsedTime < 17) {
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+}
+
+function displayMessage(string) {
+  document.getElementById("greeting").innerText = string;
+}
+
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
